a44e36af3d6153dd9ed41b3c68f87cb0d600fbd2
MANIDEEP NAMBURI <namburimanideep@gmail.com>
2026-07-21 14:02:52 -0700

    Add Notes column to InventoryLog table

---
+++ Tables/InventoryLog.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[InventoryLog] (
+    [LogID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [ProductID] INT NOT NULL,
+    [ChangeDate] DATETIME2 NOT NULL DEFAULT GETDATE(),
+    [QuantityChange] INT NOT NULL,
+    [ChangeType] NVARCHAR(20) NOT NULL,
+    [Notes] NVARCHAR(200) NULL
+);
